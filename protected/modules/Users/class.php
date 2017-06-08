491927667eae58ce7c038afd65bfe9f5eaff5e1f
Tendors <user337100@gmail.com>
2017-06-08 12:25:09 +0300

    fix

--- protected/modules/Users/class.php
+++ protected/modules/Users/class.php
@@ -2209,7 +2209,7 @@ class Users {
         if (isset($_POST['email']) && filter_var($_POST['email'], FILTER_VALIDATE_EMAIL) === false) {
             $out['status'] = 'error';
             $out['errors'][] = 1;
-        } else if (!APP::Module('DB')->Select($this->settings['module_users_db_connection'], ['fetchColumn', 0], ['id'], 'users', [['email', '=', $_POST['email'], PDO::PARAM_STR]])) {
+        } else if (!isset($_POST['email']) || !APP::Module('DB')->Select($this->settings['module_users_db_connection'], ['fetchColumn', 0], ['id'], 'users', [['email', '=', $_POST['email'], PDO::PARAM_STR]])) {
             $out['status'] = 'error';
             $out['errors'][] = 2;
         }
